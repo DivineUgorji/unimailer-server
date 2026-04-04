c0b3c520990b61090127f2ae89c865eb3973c944
Divine Ugorji <divineugorji37@gmail.com>
2026-04-04 05:45:59 +0100

    fixes

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,59 +1,16 @@
-// export default [
-//   "strapi::logger",
-//   "strapi::errors",
-//   // {
-//   //   name: "strapi::cors",
-//   //   config: {
-//   //     origin: ["http://localhost:3000", "https://your-app.vercel.app"],
-//   //     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
-//   //     headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-//   //   },
-//   // },
-//   "strapi::security",
-//   "strapi::cors",
-//   "strapi::poweredBy",
-//   "strapi::query",
-//   "strapi::body",
-//   "strapi::session",
-//   "strapi::favicon",
-//   "strapi::public",
-// ];
-
 export default [
   "strapi::logger",
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "res.cloudinary.com", // 👈 add this
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "res.cloudinary.com", // 👈 add this
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  {
-    name: "strapi::cors",
-    config: {
-      origin: ["http://localhost:3000", "https://your-app.vercel.app"],
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-    },
-  },
+  // {
+  //   name: "strapi::cors",
+  //   config: {
+  //     origin: ["http://localhost:3000", "https://your-app.vercel.app"],
+  //     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+  //     headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+  //   },
+  // },
+  "strapi::security",
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
@@ -61,3 +18,39 @@ export default [
   "strapi::favicon",
   "strapi::public",
 ];
+/////////////////////////////////
+// export default [
+//   "strapi::logger",
+//   "strapi::errors",
+//   {
+//     name: "strapi::security",
+//     config: {
+//       contentSecurityPolicy: {
+//         useDefaults: true,
+//         directives: {
+//           "connect-src": ["'self'", "https:"],
+//           "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+//           "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+//           upgradeInsecureRequests: null,
+//         },
+//       },
+//     },
+//   },
+//   {
+//     name: "strapi::cors",
+//     config: {
+//       origin: [
+//         "http://localhost:3000",
+//         "https://unimailer-server-app.onrender.com",
+//       ],
+//       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+//       headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+//     },
+//   },
+//   "strapi::poweredBy",
+//   "strapi::query",
+//   "strapi::body",
+//   "strapi::session",
+//   "strapi::favicon",
+//   "strapi::public",
+// ];
